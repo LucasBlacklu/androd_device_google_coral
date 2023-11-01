79055462f19faf3da8ba7eeca9555e78e2134c62
LucasBlackLu <lucasblack997@gmail.com>
2023-11-02 02:59:36 +0800

    floral: Add Build flag

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -17,6 +17,13 @@
 include build/make/target/board/BoardConfigMainlineCommon.mk
 include build/make/target/board/BoardConfigPixelCommon.mk
 
+# Build
+ALLOW_MISSING_DEPENDENCIES=true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 TARGET_BOARD_PLATFORM := msmnile
 TARGET_BOARD_INFO_FILE := device/google/coral/board-info.txt
 USES_DEVICE_GOOGLE_CORAL := true
