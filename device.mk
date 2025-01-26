aeaed3ca2066a41f17c3f5ae16932af9d9517f3d
LucasBlackLu <lucasblack997@gmail.com>
2025-01-27 05:41:44 +0800

    Revert "flora: Build ElmyraService"
    * It crash now , let's import it when we build pixel-franework nect time
    
    This reverts commit 64ef9f03e77ba00d894c75e98429d598c400bc59.

--- device.mk
+++ device.mk
@@ -219,10 +219,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
-# ElmyraService 
-PRODUCT_PACKAGES += \
-	ElmyraService
-
 # Audio Features
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.feature.external_dsp.enable=true \
